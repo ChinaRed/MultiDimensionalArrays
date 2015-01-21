6d81251bb671cf7f386829a5248710dc333d4f0d
Jon <jonathan.tan83@gmail.com>
2015-01-21 09:38:34 -1000

    input chai spec to test-generator.js

--- test/test-generator.js
+++ test/test-generator.js
@@ -1,1 +1,13 @@
-
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+ 
+var mda = require('../MultidimensionalArray.js');
+ 
+describe('something', function() {
+  it('should do something', function() {
+    var something = "some value";
+    expect(something).to.be.a('string');
+    something.should.equal('some value');
+  });
+});
