0a00d307409e1a3fc64673ab0ab1867164b8ae19
Jon <jonathan.tan83@gmail.com>
2015-01-22 15:47:49 -1000

    commit to start branch level2

--- test/test-counter.js
+++ test/test-counter.js
@@ -3,13 +3,28 @@ var expect = chai.expect;
 var should = chai.should();
 var mda = require('../MultidimensionalArray.js');
  
-describe('counter', function() {
-  it('Return a number representing how many truthy values are inside the argument, sample_array', function() {
+describe('count', function() {
+  it('Returns a number representing how many truthy values are inside the argument, sample_array', function() {
     
-    var sample_array = [ 0, 1, 1, 0, 1, 0, 1, 1];
-    var result = mda.counter(sample_array);
+    var sample_array = [ 0, 1, 2, 3, 2, 1, 0, -1];
+    var result = mda.count();
+    var emptyArr = [];
 
-    expect(result).to.equal(5);
+    count.filter(function(array){
+      if (array() > 0){
+        then(array() === true);
+      }
+      expect(array).to.be.a(number);
+      expect(array).to.be(true);
 
+      if (array(i) === true){
+        emptyArr.push(i);
+      }
+
+    });
+
+    
+    expect(result).to.be.a(Number);
+    result.should.equal(10);
   });
 });
